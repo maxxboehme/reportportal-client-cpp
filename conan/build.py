a074a60fff68b5cc1faff0b7b6a1269248738609
Maxx Boehme <maxxboehme@gmail.com>
2020-06-16 23:55:40 -0500

    Making build.py only build static

--- conan/build.py
+++ conan/build.py
@@ -2,6 +2,6 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="False")
+    builder = ConanMultiPackager(options=["reportportal-client-cpp:shared"=False])
+    builder.add_common_builds()
     builder.run()
