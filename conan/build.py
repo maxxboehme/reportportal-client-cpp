5ba05e437092a15e97a2fe88b712a4cd04b56dc8
Maxx Boehme <maxxboehme@gmail.com>
2020-06-18 19:57:32 -0500

    archs should be a list in build.py

--- conan/build.py
+++ conan/build.py
@@ -3,7 +3,7 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(
-        archs="x86_64",
+        archs=["x86_64"],
         build_policy="missing")
     builder.add_common_builds(shared_option_name=False)
     builder.run()
